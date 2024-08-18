0326363fe051ca0407365ab47df66e3e9bfa3e4a
MerciCH <113984015+MerciCH@users.noreply.github.com>
2024-08-18 16:12:23 +0800

    Try resolve link path problem 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    reactStrictMode: true,
     assetPrefix: "https://xushengxianggg.github.io/xushengxiang.github.io",
-    basePath: "",
+    basePath: "/xushengxiang.github.io",
 };
 
 export default nextConfig;
