b521700095c516c1418d740245af409bad103ff2
MerciCH <113984015+MerciCH@users.noreply.github.com>
2024-08-19 14:54:44 +0800

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    assetPrefix: "https://xushengxianggg.github.io/xushengxiang.github.io",
+    assetPrefix: "/xushengxiang.github.io",
     basePath: "/xushengxiang.github.io",
 };
 
