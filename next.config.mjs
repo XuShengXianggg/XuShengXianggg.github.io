d599be31120867dd3a440391bdb8958df296b542
MerciCH <113984015+MerciCH@users.noreply.github.com>
2024-08-19 15:23:03 +0800

    Try resolve path problem

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    assetPrefix: "/XuShengXianggg.github.io",
-    basePath: "/XuShengXianggg.github.io",
+    // assetPrefix: "/XuShengXianggg.github.io",
+    // basePath: "/XuShengXianggg.github.io",
 };
 
 export default nextConfig;
